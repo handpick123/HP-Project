a5c652ed816fcff8e78f6c2ce2ddc34994c1f0f7
hieulam1312 <hieulam1312@gmail.com>
2021-08-17 16:06:45 +0700

    Update test.py

--- test.py
+++ test.py
@@ -199,18 +199,13 @@ def main():
             or_result=0
             with col1:
                 if not id_or:
-                    st.markdown('')
-                    st.write(order_df)
-                    list_order=order_df['ID_ORDER'].unique().tolist()
-                    list_bp=order_df['Bộ_Phận'].unique().tolist()
-                    order_df
+                    or_result=order_df
                 else:
-                    order_df.info()
                     or_result=order_df.loc[order_df['ID_ORDER'].str.contains(id_or,na=False)]
-                    list_order=or_result['ID_ORDER'].unique().tolist()
-                    list_bp=or_result['Bộ_Phận'].unique().tolist()
-                    st.markdown('')
-                    st.write(or_result)
+                list_order=or_result['ID_ORDER'].unique().tolist()
+                list_bp=or_result['Bộ_Phận'].unique().tolist()
+                st.markdown('')
+                st.write(or_result)
             
             with col2:
                     for l in list_bp:
