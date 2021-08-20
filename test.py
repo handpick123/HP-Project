be4b1c12a44c0d2514a5c2fc11adae17a4fd0c7e
hieulam1312 <hieulam1312@gmail.com>
2021-08-20 13:10:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,88 +7,94 @@ import pandas as pd #-> Để update data dạng bản
 from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
 import seaborn as sns
 def created_data():
-                ## Collect QR scan database from Googlesheet
-        credentials = service_account.Credentials.from_service_account_info(
-            st.secrets["gcp_service_account"],
-            scopes=['https://spreadsheets.google.com/feeds',
-                'https://www.googleapis.com/auth/drive'],)
+
+## Collect QR scan database from Googlesheet
+        scope = ['https://spreadsheets.google.com/feeds',
+                'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            'speedy-precept-314204-30d59f2d7609.json', scope)
         gc1 = gspread.authorize(credentials)
         spreadsheet_key = '1VakpJ7a7o1Eosyh-wto80X0TrkTYSxfaEwNoza3KlLw' # input SPREADSHEET_KEY HERE
-        spreadsheet_key='1DHvhU43JWaeODEUGel9JknkgVJWBen1RNtzRhViq93g'
+
         #syntax_df
         sh1 = gc1.open("HP - Hist").worksheet('Syntaxs')
         syntaxs_=sh1.get_all_records()
         syntaxs_df=pd.DataFrame(syntaxs_)
-        process_syntax=syntaxs_df 
+        process_syntax=syntaxs_df #[~syntaxs_df["Step"].str.contains('HP', na=False)]
 
         sh2=gc1.open('HP - Hist').worksheet('Category')
         category=sh2.get_all_records()
         category_df=pd.DataFrame(category)
-
+        sh4=gc1.open('Handpick - Đơn đặt hàng').worksheet('1. DON HANG')
+        order=sh4.get_all_records()
+        order_=pd.DataFrame(order)
+        order_=order_.drop(columns={'KHÁCH HÀNG','NHÓM','ĐVT','QUI CÁCH','ĐÓNG GÓI','LOẠI QC','GHI CHÚ','NMSX','LOẠI HÀNG','GỖ','SƠN','NỆM','TÊN TTF','NGÀY LẬP','SỐ ĐƠN HÀNG'},axis=0)
+        order_['S/L']=order_['S/L'].astype('str')
+        order_.columns=order_.columns.str.replace(" ","_")    
+        order_['Order Category 3']=order_['KHUNG']+order_['KIM_LOẠI']+order_['VENEER']
+        sub_order=order_[['ID_ORDER','Order Category 3']]
+        sub_order_=sub_order.merge(category_df,how='left',on='Order Category 3')
+        sub_order_=sub_order_[['ID_ORDER','ID','Descriptions']]
+        order_df=order_.merge(sub_order_,how='left',on='ID_ORDER')
+        order_df=order_df[['ID_ORDER','TÊN_HANDPICK','S/L','NGÀY_XUẤT','ID','Descriptions']]
 
         sh3=gc1.open('HP - Hist').worksheet('Form')
         Form=sh3.get_all_records()
         Form_df=pd.DataFrame(Form)
         Form_df.columns=Form_df.columns.str.replace(" ","_")
 
-        sh4=gc1.open('Handpick - Đơn đặt hàng').worksheet('1. DON HANG')
-        order=sh4.get_all_records()
-        order_df=pd.DataFrame(order)
-        order_df=order_df.drop(columns={'KHÁCH HÀNG','KHUNG','KIM LOẠI','VÁN ÉP','VENEER','NHÓM','ĐVT','QUI CÁCH','ĐÓNG GÓI','LOẠI QC','GHI CHÚ','NMSX','LOẠI HÀNG','GỖ','SƠN','NỆM','TÊN TTF','NGÀY LẬP','SỐ ĐƠN HÀNG'},axis=0)
-        order_df['S/L']=order_df['S/L'].astype('str')
-        order_df['ID ORDER']=order_df['ID ORDER'].astype('str')
-        order_df.columns=order_df.columns.str.replace(" ","_")       
-
         data=Form_df.set_index(['Dấu_thời_gian','Thao_tác_của_bạn'])
         data.columns=data.columns.str.split('-', expand=True)
-
 
         data_df=data.stack().reset_index().rename(columns={'level_2':'state'})
         data_df=data_df.replace("",np.nan)
         data_df=data_df.loc[data_df['QUÉT_MÃ_ĐHM'].isnull()==False ]
-        data_v1=data_df
-        data_v1[['BỘ_PHẬN', 'ID','NHÀ_MÁY']] = data_v1['Thao_tác_của_bạn'].str.split('-', 2, expand=True)
-        # data_v1.head()
 
-        data_v2=data_v1.merge(category_df,how='left',on='ID')
-        data_v2=data_v2.sort_values(by=['QUÉT_MÃ_ĐHM','BỘ_PHẬN','Dấu_thời_gian'])
+        data_v1=data_df.copy()
+        data_v1[['BỘ_PHẬN', 'ID','NHÀ_MÁY']] = data_v1['Thao_tác_của_bạn'].str.split('-', 2, expand=True)
+        data_v1=data_v1.rename(columns={'QUÉT_MÃ_ĐHM':'ID_ORDER'})
+
+        data_v2=data_v1.merge(sub_order_,on='ID_ORDER',how='left')
+        data_v2=data_v2.sort_values(by=['ID_ORDER','BỘ_PHẬN','Dấu_thời_gian'])
         data_v3=data_v2.replace("",np.nan).ffill(axis = 0).reset_index()
         data_v=data_v3.merge(process_syntax,how='left',on='Thao_tác_của_bạn')
         data_v4=data_v[data_v['Bộ_phận'].str.contains('D')==False]
-        list_order=data_v4['QUÉT_MÃ_ĐHM'].unique().tolist()
+
+        list_order=data_v4['ID_ORDER'].unique().tolist()
         _list={}
         for i in list_order:
             _list[i]={}
-            _list[i]['Thời_gian']=data_v4.loc[data_v4.QUÉT_MÃ_ĐHM==i]['Dấu_thời_gian'].to_list()
-            _list[i]['Bước']=data_v4.loc[data_v4.QUÉT_MÃ_ĐHM==i]['BỘ_PHẬN'].to_list()
-            _list[i]['Bộ_Phận']=data_v4.loc[data_v4.QUÉT_MÃ_ĐHM==i]['Bộ_phận'].to_list()
-            _list[i]['Tình_trạng']=data_v4.loc[data_v4.QUÉT_MÃ_ĐHM==i]['Mô_Tả'].to_list()
-            _list[i]['Nhóm_ĐH']=data_v4.loc[data_v4.QUÉT_MÃ_ĐHM==i]['Order Category 1'].to_list()
-            _list[i]['Nhóm_ThuMua']=data_v4.loc[data_v4.QUÉT_MÃ_ĐHM==i]['Order Category 3'].to_list()
-            _list[i]['Nhà_máy']=data_v4.loc[data_v4.QUÉT_MÃ_ĐHM==i]['NHÀ_MÁY'].to_list()
+            _list[i]['Thời_gian']=data_v4.loc[data_v4.ID_ORDER==i]['Dấu_thời_gian'].to_list()
+            _list[i]['Bước']=data_v4.loc[data_v4.ID_ORDER==i]['BỘ_PHẬN'].to_list()
+            _list[i]['Bộ_Phận']=data_v4.loc[data_v4.ID_ORDER==i]['Bộ_phận'].to_list()
+            _list[i]['Tình_trạng']=data_v4.loc[data_v4.ID_ORDER==i]['Mô_Tả'].to_list()
+            # _list[i]['Nhóm_ĐH']=data_v4.loc[data_v4.ID_ORDER==i]['Order Category 1'].to_list()
+            _list[i]['Nhóm_ThuMua']=data_v4.loc[data_v4.ID_ORDER==i]['Descriptions'].to_list()
+            _list[i]['Nhà_máy']=data_v4.loc[data_v4.ID_ORDER==i]['NHÀ_MÁY'].to_list()
+
+        new_={k:{sk:sv[-1] for sk,sv in s.items() if len(sv)>0} for k,s in _list.items() }
+        new_status=pd.DataFrame.from_dict(new_, orient='index').reset_index()
+        new_status['Bước']=new_status['Bước'].astype(str).astype(int)
 
         new_={k:{sk:sv[-1] for sk,sv in s.items() if len(sv)>0} for k,s in _list.items() }
         new_status=pd.DataFrame.from_dict(new_, orient='index').reset_index()
         new_status=new_status.rename(columns={'index':'ID_ORDER','Bước':'STEP'})
         order_df_=order_df.merge(new_status,how='left',on='ID_ORDER')
-        order_df_f=order_df_.drop(columns={'Thời_gian','STEP','NGÀY_XUẤT','Nhóm_ĐH'})
+        order_df_f=order_df_.drop(columns={'Thời_gian','STEP','NGÀY_XUẤT'})
         D_=data_v.loc[data_v['Bộ_phận'].str.contains('D')==True]
-        tm_order=D_['QUÉT_MÃ_ĐHM'].unique().tolist()
+        tm_order=D_['ID_ORDER'].unique().tolist()
         tm_list={}
         for j in tm_order:
             tm_list[j]={}
-            tm_list[j]['Thời_gian']=D_.loc[D_.QUÉT_MÃ_ĐHM==j]['Dấu_thời_gian'].to_list()
-            tm_list[j]['Bộ_Phận']=D_.loc[D_.QUÉT_MÃ_ĐHM==j]['Bộ_phận'].to_list()
-            tm_list[j]['Tình_trạng']=D_.loc[D_.QUÉT_MÃ_ĐHM==j]['Mô_Tả'].to_list()     
+            tm_list[j]['Thời_gian']=D_.loc[D_.ID_ORDER==j]['Dấu_thời_gian'].to_list()
+            tm_list[j]['Bộ_Phận']=D_.loc[D_.ID_ORDER==j]['Bộ_phận'].to_list()
+            tm_list[j]['Tình_trạng']=D_.loc[D_.ID_ORDER==j]['Mô_Tả'].to_list()     
 
         tm_df={k2:{sk2:sv2[-1] for sk2,sv2 in s2.items() if len(sv2)>0} for k2,s2 in tm_list.items() }
         tm_df_=pd.DataFrame.from_dict(tm_df, orient='index').reset_index()
-        if tm_df_.empty:
-            order_D_=""
-        else:
-            tm_df_=tm_df_.rename(columns={'index':'ID_ORDER','Bước':'STEP'})
-            order_D=tm_df_.merge(order_df,how='left',on='ID_ORDER')
-            order_D_=order_D[['ID_ORDER','TÊN_HANDPICK','Tình_trạng']]
+        tm_df_=tm_df_.rename(columns={'index':'ID_ORDER','Bước':'STEP'})
+        order_D=tm_df_.merge(order_df,how='left',on='ID_ORDER')
+        order_D_=order_D[['ID_ORDER','TÊN_HANDPICK','Tình_trạng']]
         return new_status,order_df_f,order_D_
 st.set_page_config(layout='wide')
 st.markdown("<h1 style='text-align: center; color: blue;font-style:bold'>OPERATION DASHBOARD</h1>", unsafe_allow_html=True)
@@ -103,6 +109,7 @@ def main():
             list=created_data()
             last_status=list[0]
             order_df=list[1]
+            order_df=order_df.drop(columns={'ID','Descriptions'})
             D=list[2]
             c1_1,c1_2=st.columns((2.5,2))
             c1,c2,c3 = st.columns((1.125,1.125,1.75))
@@ -128,15 +135,15 @@ def main():
                 st.markdown('')
                 st.write(or_result)
             r3_1,r3_2,r3_3,r3_4=st.columns((1.25,1.25,1,1))
-            with r3_1:
-                st.markdown("<h4 style='text-align: left; color: blue;font-style:bold'>D. Thu mua</h1>",unsafe_allow_html=True)
-                D
-                for l in range(0,round(len(list_bp)/2)):
+            for l in range(0,round(len(list_bp)/2)):
+                with r3_1:
                     st.markdown("<h4 style='text-align: left; color: blue;font-style:bold'>{}</h1>".format(list_bp[l]),unsafe_allow_html=True)
                     st.markdown('')
                     bp_df=or_result[or_result['Bộ_Phận']==list_bp[l]].reset_index()
                     bp_df_=bp_df[['ID_ORDER','TÊN_HANDPICK','Tình_trạng']]
                     bp_df_
+                    st.markdown("<h4 style='text-align: left; color: blue;font-style:bold'>D. Thu mua</h1>",unsafe_allow_html=True)
+                    D
             for m in range(round(len(list_bp)/2),len(list_bp)):
                 with r3_2:
                     st.markdown("<h4 style='text-align: left; color: blue;font-style:bold'>{}</h1>".format(list_bp[m]),unsafe_allow_html=True)
