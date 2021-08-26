9fb86d2dee33acde7548c14c286f57b9c4febfad
hieulam1312 <hieulam1312@gmail.com>
2021-08-26 13:33:44 +0700

    Update test.py

--- test.py
+++ test.py
@@ -129,20 +129,7 @@ def color_survived(val):
         'sai': 'green',
     }).fillna('') # a fallback for fruits we haven't colorized
     return 'background-color:' + bg_color
-def download_link(object_to_download, download_filename, download_link_text):
 
-    if isinstance(object_to_download,pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index = False, header=True,encoding="cp1258")
-            
-    # some strings <-> bytes conversions necessary here
-        b64 = base64.b64encode(object_to_download.encode()).decode()
-
-    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
-
-
-import streamlit as st
-import base64
-from io import BytesIO
 
 def to_excel(df):
     output = BytesIO()
