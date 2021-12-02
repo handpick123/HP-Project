62a7849b5632a82ac4a1dfb987791c1d0b0ee7d5
handpick123 <90091597+handpick123@users.noreply.github.com>
2021-12-02 08:25:21 +0700

    Update test.py

--- test.py
+++ test.py
@@ -368,7 +368,7 @@ def main():
                 elif cho=='ĐH đang thiếu/sai':
                     file=or_result[or_result['Tình_trạng'].str.contains('thiếu/sai')]
                 else:
-                    file=or_result[or_result['Tình_trạng'].str.contains('đang')]
+                    file=or_result1
                 st.markdown("")
                 tmp_download_link = download_link(file, 'YOUR_DF.csv', 'Bấm vào đây để tải danh sách!')
                 st.markdown(tmp_download_link, unsafe_allow_html=True)
